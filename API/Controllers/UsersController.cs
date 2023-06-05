ba9d351a39d86f182ffed33c45038c1cc99722c1
Vishnu B <vishnubmp@gmail.com>
2023-06-05 10:42:28 +0530

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,9 +9,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[Controller]")]
-    public class UsersController:ControllerBase
+    
+    public class UsersController:BaseApiController
     {
         private readonly DataContext _context;
 
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+
+using System.Reflection.Metadata.Ecma335;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt=>
+            {
+                opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;    
+        }
+    }
+}
